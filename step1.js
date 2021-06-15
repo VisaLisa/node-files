fba362cdf167bd9065c7b54df43ec75427e77cbe
Lisa Jiamsirioungkul <visakhalisajiam@gmail.com>
2021-06-15 13:55:51 -0700

    step1

--- step1.js
+++ step1.js
@@ -1,7 +1,19 @@
-const repeatingSent = () => {
-    for (let i =0; i < 10; i++) {
-        console.log("HELLO FROM NODE!");
+const fs = require('fs');
+const process = require('process');
+
+/** read file at path and print it out. */
+
+function cat(path) {
+    //   read a file, error-check, print out file
+  fs.readFile(path, 'utf8', function(err, data) {
+    if (err) {
+      console.error(`Error reading ${path}: ${err}`);
+      process.exit(1);
+    } else {
+      console.log(data);
     }
+  });
 }
 
-repeatingSent();
+cat(process.argv[2]);
+// cat('one.txt');
